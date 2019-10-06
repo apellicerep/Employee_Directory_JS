d3dade2a57909f2718b0048b30479d4b6e195215
Arturo <arturomailbcn@gmail.com>
2019-10-06 14:11:33 +0200

    modal working full

--- js/scripts.js
+++ js/scripts.js
@@ -32,15 +32,14 @@ async function mainPage() {
 
     }).join('')
     document.querySelector('#gallery').innerHTML = ourHtmlProfiles;
-    return ourHtmlProfiles;
+
 }
 
 
 
 
 document.querySelector('#gallery').addEventListener('click', (e) => {
-    //console.log(e.target)
-    let html = "";
+
     let divCard = e.target;
     if (e.target.className !== 'gallery') {
 
@@ -50,44 +49,84 @@ document.querySelector('#gallery').addEventListener('click', (e) => {
                 divCard = e.target.parentNode.parentNode
             }
         }
-        console.log(divCard.getAttribute("myId"))
-
         posPeopleJson = divCard.getAttribute("myId")
-        let person = peopleJson.results[posPeopleJson]
-        let firstName = person.name.first;
-        let lastName = person.name.last;
-        let email = person.email;
-        let city = person.location.city
-        let cell = person.cell
-        let streetName = person.location.street.name
-        let numberStreet = person.location.street.number
-        let postCode = person.location.postcode
-        let dob = person.dob.date.slice(0, 9).replace(/-/g, "/")
-        let img = person.picture.medium
-
-        html = `<div class="modal-container">
-                <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src="${img}" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3>
-                        <p class="modal-text">${email}</p>
-                        <p class="modal-text cap">${city}</p>
-                        <hr>
-                        <p class="modal-text">${cell}</p>
-                        <p class="modal-text">${numberStreet} ${streetName}. ${postCode}</p>
-                        <p class="modal-text">Birthday: ${dob}</p>
-                    </div>
-                </div>`
-        console.log(html)
-        document.querySelector('body').insertAdjacentHTML("afterend", html)
-
+        createModalCard(posPeopleJson)
 
     }
 
 })
 
 
-mainPage().then((html) => console.log(peopleJson))
+mainPage()
+
+
+function createModalCard(numCard) {
+    let person = peopleJson.results[numCard]
+    console.log(numCard)
+    let firstName = person.name.first;
+    let lastName = person.name.last;
+    let email = person.email;
+    let city = person.location.city
+    let cell = person.cell
+    let streetName = person.location.street.name
+    let numberStreet = person.location.street.number
+    let postCode = person.location.postcode
+    let dob = person.dob.date.slice(0, 9).replace(/-/g, "/")
+    let img = person.picture.medium
+    let html = "";
+
+    html = `<div class="modal-container" myId="${numCard}">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${img}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3>
+                    <p class="modal-text">${email}</p>
+                    <p class="modal-text cap">${city}</p>
+                    <hr>
+                    <p class="modal-text">${cell}</p>
+                    <p class="modal-text">${numberStreet} ${streetName}. ${postCode}</p>
+                    <p class="modal-text">Birthday: ${dob}</p>
+                </div>
+            </div>
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
+            </div>`;
+    document.querySelector('.gallery').insertAdjacentHTML("afterend", html)
+
+}
+
+
+document.addEventListener('click', (e) => {
+
+    if (e.target.id === "modal-close-btn" || ((e.target.tagName === "STRONG") && (e.target.textContent === "X"))) {
+        document.querySelector('.gallery').nextElementSibling.remove()
+    }
+    else if (e.target.id === "modal-prev") {
+        console.log(e.target.parentNode.parentNode.getAttribute('myId'))
+        let numCard = e.target.parentNode.parentNode.getAttribute('myId');
+
+        if (numCard > 0) {
+            document.querySelector('.gallery').nextElementSibling.remove()
+            numCard--
+            createModalCard(numCard)
+        }
+
+    } else if (e.target.id === "modal-next") {
+        let numCard = e.target.parentNode.parentNode.getAttribute('myId');
+
+        if (numCard < peopleJson.results.length - 1) {
+            document.querySelector('.gallery').nextElementSibling.remove()
+            numCard++
+            createModalCard(numCard)
+        }
+    }
+
+})
+
+
+
 
 
